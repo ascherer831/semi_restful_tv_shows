0ff952dbf470d3641feb44e577a6950227105be4
Archer Scherer <ascherer831@gmail.com>
2021-06-10 14:49:53 -0500

    validations added

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -1,6 +1,42 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class ShowManager(models.Manager):
+    #validator for the /create path
+    def create_validator(self,postData):
+        errors= {}
+        today = str(datetime.date.today())
+        match = Show.objects.filter(title=postData['title'])
+        if len(postData['title']) < 2:
+            errors['name'] = "Show title must be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network name must be at least 3 characters"
+        if len(postData['description']) < 10:
+            if len(postData['description']) >0:
+                errors['description'] = "Description must be greater than 10 charaters or left blank"
+        if postData['release_date'] >= today:
+            errors['reelease_date'] = "Release date must be in the past"
+        if len(match) > 0:
+            errors['exists'] = "This show already exists"
+        return errors
+    
+    def update_validator(self, postData):
+        errors= {}
+        today = str(datetime.date.today())
+        if len(postData['title']) < 2:
+            errors['name'] = "Show title must be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network name must be at least 3 characters"
+        if len(postData['description']) < 10:
+            if len(postData['description']) >0:
+                errors['description'] = "Description must be greater than 10 charaters or left blank"
+        if postData['release_date'] >= today:
+            errors['reelease_date'] = "Release date must be in the past"
+        return errors
+
+
+
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -9,3 +45,5 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # overriding .manager
+    objects= ShowManager()
--- tv_shows_app/views.py
+++ tv_shows_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Show
 
 # Create your views here.
@@ -16,8 +17,14 @@ def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    this_show= Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'],description=request.POST['description'])
-    return redirect(f'/shows/{this_show.id}')
+    errors = Show.objects.create_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request,value)
+        return redirect('/shows/new')
+    else:
+        this_show= Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'],description=request.POST['description'])
+        return redirect(f'/shows/{this_show.id}')
 
 def info(request,id):
     context = {'this_show':Show.objects.get(id=id)}
@@ -31,13 +38,18 @@ def edit(request,id):
 
 def update(request, id):
     update_show = Show.objects.get(id=id)
-    update_show.title = request.POST['title']
-    update_show.network = request.POST['network']
-    update_show.release_date = request.POST['release_date']
-    update_show.description = request.POST['description']
-    update_show.save()
-
-    return redirect(f'/shows/{update_show.id}')
+    errors=Show.objects.update_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{update_show.id}/edit')
+    else:
+        update_show.title = request.POST['title']
+        update_show.network = request.POST['network']
+        update_show.release_date = request.POST['release_date']
+        update_show.description = request.POST['description']
+        update_show.save()
+        return redirect(f'/shows/{update_show.id}')
 
 def delete(request,id):
     dead_show = Show.objects.get(id=id)
